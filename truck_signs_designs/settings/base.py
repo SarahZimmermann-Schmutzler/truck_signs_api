cc98989c076d34c59e8bbd2c6688522fce54adaf
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-11-08 12:10:32 +0100

    new new new path to .env in base.py

--- truck_signs_designs/settings/base.py
+++ truck_signs_designs/settings/base.py
@@ -26,7 +26,7 @@ env = environ.Env()
 # reading env file
 env_file_path = os.path.join(BASE_DIR, 'truck_signs_designs', 'settings', '.env')
 # env_file_path = os.path.join(BASE_DIR, '.env')
-environ.Env.read_env(env_file_path)
+env.read_env(env_file_path)
 
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', env('IP_ADDRESS_VM')]
