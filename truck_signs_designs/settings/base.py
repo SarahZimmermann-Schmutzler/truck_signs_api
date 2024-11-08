0136d19cae7759958888dc7b353e756edee1f1db
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-11-08 11:50:23 +0100

    new new path to .env in base.py

--- truck_signs_designs/settings/base.py
+++ truck_signs_designs/settings/base.py
@@ -24,8 +24,8 @@ TEMPLATES_DIR = os.path.join(ROOT_BASE_DIR,'templates')
 
 env = environ.Env()
 # reading env file
-# env_file_path = os.path.join(BASE_DIR, 'truck_signs_designs', 'settings', '.env')
-env_file_path = os.path.join(BASE_DIR, '.env')
+env_file_path = os.path.join(BASE_DIR, 'truck_signs_designs', 'settings', '.env')
+# env_file_path = os.path.join(BASE_DIR, '.env')
 environ.Env.read_env(env_file_path)
 
 
