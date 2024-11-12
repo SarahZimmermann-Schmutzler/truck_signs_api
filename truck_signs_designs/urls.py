5dc2bfcef0356b21ea38f8207110500616faf929
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-11-12 10:34:45 +0100

    new path to static

--- truck_signs_designs/urls.py
+++ truck_signs_designs/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # url(r'^$', HomePageAPI, name='home'),
     url(r'^truck-signs/', include('backend.urls', namespace='trucks-signs-namespace')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
